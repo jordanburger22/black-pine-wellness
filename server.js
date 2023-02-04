6159f5de311b4b30cfd92fc900fb454650bc0302
Jordan Burger <jordanburger22@gmail.com>
2023-02-03 18:41:06 -0700

    added server functions for render

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const {expressjwt} = require('express-jwt')
+const path = require("path")
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -13,7 +14,7 @@ mongoose.connect("mongodb+srv://jordanburger22:.5HC5.FQHsqYVz8@cluster0.cihycu0.
 
 // app.use('/admin', expressjwt({secret: process.env.SECRET, algorithms: ['HS256']}))
 
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use('/services', require('./routes/services.js'))
 app.use('/businessinfo', require('./routes/businessInfo.js'))
 app.use('/massagestyles', require('./routes/massageStyles.js'))
@@ -27,6 +28,11 @@ app.use((err, req, res, next) => {
         res.status(err.status)
     }
     return res.send({errMsg: err.message})
+})
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
 app.listen(5555, () => {
